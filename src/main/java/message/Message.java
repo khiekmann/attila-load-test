787296ea07eaa259127591ab52c0e6e1ac42c21e
Heinz Karsten Hiekmann <k.hiekmann@andagon.com>
2016-12-07 07:39:48 +0100

    commit after a long time

--- src/main/java/message/Message.java
+++ src/main/java/message/Message.java
@@ -2,7 +2,34 @@ package message;
 
 /**
  * Created by HiekmaHe on 01.12.2016.
+ *
+ * SRP: Message superclass. Content will be sent.
  */
 public class Message
 {
+	private String content;
+
+	public Message(String content)
+	{
+		this.content = content;
+	}
+
+	@Override
+	public boolean equals(Object other) {
+		boolean isEqual = false;
+		if (other instanceof Message) {
+			isEqual = (this.hashCode() == other.hashCode());
+		}
+		return isEqual;
+	}
+
+	@Override
+	public int hashCode() {
+		return content.hashCode();
+	}
+
+	@Override
+	public String toString() {
+		return content;
+	}
 }
